2ee7820602004fee6bc4b89fad8c712b05b1984f
zhoujm <1820732570@qq.com>
2022-04-06 23:17:56 +0800

    更新文档

--- src/config/index.js
+++ src/config/index.js
@@ -2,5 +2,13 @@ export default {
     baseUrl: {
         dev: '/api',
         pro: ''
-    }
+    },
+    // dev: {
+    //     host: '0.0.0.0',
+    //     port: 8080,
+    //     autoOpenBrowser: true,
+    //     errorOverlay: true,
+    //     notifyOnErrors: false,
+    //     poll: false,
+    // }
 }
